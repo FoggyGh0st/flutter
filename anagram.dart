281abcb006d2082db3957660d392f1349e773014
FoggyGh0st <113252795+FoggyGh0st@users.noreply.github.com>
2022-12-17 21:30:15 +0300

    Add files via upload

---
+++ anagram.dart
@@ -0,0 +1,19 @@
+anagram<T>(String word, String word2) {
+  word = word.toUpperCase();
+  word2 = word2.toUpperCase();
+  if (word.substring(0, 1) != word2.substring(word.length - 1, 1)) {
+    return false;
+  }
+  if (word.length < 2) {
+    return true;
+  }
+  word = word.substring(1, 1);
+  word2 = word.substring(word2.length - 1, 1);
+  return anagram(word, word2);
+}
+
+void main() {
+  anagram("finder", "Friend");
+  anagram("hello", "bye");
+  anagram("f? inder", "Friend");
+}
